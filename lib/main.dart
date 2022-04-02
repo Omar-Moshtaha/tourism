27d293b466bef9b63676fb82b46b01c8ad0172eb
Omar-Moshtaha <omar.moshtaha5@gmail.com>
2022-04-02 16:14:07 +0300

    Refund_Policy_Screen.Dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tourism/faq.dart';
 import 'package:tourism/more.dart';
+import 'package:tourism/refund_policy.dart';
 import 'package:tourism/register.dart';
 import 'package:tourism/success_page.dart';
 import 'package:tourism/success_page2.dart';
@@ -34,7 +35,7 @@ statusBarColor: Colors.white.withOpacity(0),
 
     )
   ),
-  home:FAQ(),
+  home:RefundPolicy(),
 );
   }
 
