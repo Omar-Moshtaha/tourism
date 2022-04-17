0b1406da7c7aa94ef0a09aa8ff28fa50a79fc810
Omar-Moshtaha <omar.moshtaha5@gmail.com>
2022-04-17 11:30:45 +0300

    OrderDetails

--- lib/Order Details.dart
+++ lib/Order Details.dart
@@ -22,76 +22,111 @@ class OrderDetails extends StatelessWidget {
         title: Text("Order Details",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
       ),
       body: Container(
-         child: Column(
-           crossAxisAlignment: CrossAxisAlignment.start,
-           children: [
-             Padding(
-               padding: const EdgeInsets.only(right: 20, left: 20,top:20 ),
-               child: Stack(
-                 children: [
-                   Container(
-                     width: double.infinity,
-                      height: 150,
-                     decoration: BoxDecoration(
-                       color: Colors.white,
-                       image: DecorationImage(
-                         image: AssetImage("image/image47.png"),
-                         fit: BoxFit.cover
+         child: SingleChildScrollView(
+           physics: BouncingScrollPhysics(),
+           child: Column(
+             crossAxisAlignment: CrossAxisAlignment.start,
+             children: [
+               Padding(
+                 padding: const EdgeInsets.only(right: 20, left: 20,top:20 ),
+                 child: Stack(
+                   children: [
+                     Container(
+                       width: double.infinity,
+                        height: 150,
+                       decoration: BoxDecoration(
+                         color: Colors.white,
+                         image: DecorationImage(
+                           image: AssetImage("image/image47.png"),
+                           fit: BoxFit.cover
+                         ),
+                         borderRadius: BorderRadius.circular(20)
                        ),
-                       borderRadius: BorderRadius.circular(20)
                      ),
-                   ),
         Padding(
-          padding: const EdgeInsets.only(top: 10,left: 10),
-          child: Container(child: Center(
-            child: Text("Your review",style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
+            padding: const EdgeInsets.only(top: 10,left: 10),
+            child: Container(child: Center(
+              child: Text("Your review",style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),),
+            ),width: 88,height: 20,decoration: BoxDecoration(
+              color: HexColor("#FFD24A"),
+              borderRadius: BorderRadius.circular(30)
             ),),
-          ),width: 88,height: 20,decoration: BoxDecoration(
-            color: HexColor("#FFD24A"),
-            borderRadius: BorderRadius.circular(30)
-          ),),
         ),
-                   Padding(
-                     padding: const EdgeInsets.only(top: 114,left: 260),
-                     child: Container(child: Center(
-                       child: Text("Completed",style: TextStyle(
-                         fontSize: 12,
-                         fontWeight: FontWeight.bold,
-                         color: HexColor("#3EC8BC"),
+                     Padding(
+                       padding: const EdgeInsets.only(top: 114,left: 260),
+                       child: Container(child: Center(
+                         child: Text("Completed",style: TextStyle(
+                           fontSize: 12,
+                           fontWeight: FontWeight.bold,
+                           color: HexColor("#3EC8BC"),
+                         ),),
+                       ),width: 81,height: 26,decoration: BoxDecoration(
+                           color: HexColor("#EBF8EE"),
+                           borderRadius: BorderRadius.circular(30)
                        ),),
-                     ),width: 81,height: 26,decoration: BoxDecoration(
-                         color: HexColor("#EBF8EE"),
-                         borderRadius: BorderRadius.circular(30)
-                     ),),
-                   ),
+                     ),
 
-                 ],
+                   ],
+                 ),
                ),
-             ),
-             Padding(
-               padding: const EdgeInsets.only(top: 20,left: 25,right: 25),
-               child: Text("A trip to Summit of the \nEiffel Tower in Seine River",style: TextStyle(
-                 fontWeight: FontWeight.bold,
-                 fontSize: 24,
-               ),),
-             ),
-             Padding(
-               padding: const EdgeInsets.only(left: 25,right: 25),
-               child: Text("Paris",style: TextStyle(
-                 color: HexColor("#606060"),
-                 fontSize: 14,
-               ),),
-             ),
-Row(
-  children: [
-FloatingActionButton(onPressed: () {} ,child: Image(image: AssetImage("image/image48.png"),),mini: true,)
-  ],
-)
+               Padding(
+                 padding: const EdgeInsets.only(top: 20,left: 25,right: 25),
+                 child: Text("A trip to Summit of the \nEiffel Tower in Seine River",style: TextStyle(
+                   fontWeight: FontWeight.bold,
+                   fontSize: 24,
+                 ),),
+               ),
+               Padding(
+                 padding: const EdgeInsets.only(left: 25,right: 25),
+                 child: Text("Paris",style: TextStyle(
+                   color: HexColor("#606060"),
+                   fontSize: 14,
+                 ),),
+               ),
+Padding(
+  padding: const EdgeInsets.only(left: 15,top: 20),
+  child:   Row(
 
-           ],
+    children: [
+
+
+
+      Image(image: AssetImage("image/image48.png")),
+SizedBox(width: 14,),
+Column(
+  crossAxisAlignment: CrossAxisAlignment.start,
+  children: [
+    Text("Agent Name",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
+    Padding(
+      padding: const EdgeInsets.only(bottom: 10,),
+      child: Text("Ahmed Ahmed",style: TextStyle(fontSize: 14),),
+    ),
+  ],
+),
+      Spacer(),
+      Padding(
+        padding: const EdgeInsets.only(bottom: 20,right: 25),
+        child: Text("#123daq134",style: TextStyle(
+            fontSize: 14,color: HexColor("#606060"),
+        ),),
+      ),
+
+
+    ],
+
+  ),
+),
+               Padding(
+                 padding: const EdgeInsets.only(bottom: 20),
+                 child: Container(height: 500,child: ListView.builder(shrinkWrap: true,physics: NeverScrollableScrollPhysics(),itemBuilder: (context, index) =>  BulidItem("#CBF6FF","image/image49.png","s"),itemCount: 10,)),
+               ),
+
+             ],
+           ),
          ),
         decoration: BoxDecoration(
           image: DecorationImage(
@@ -101,4 +136,27 @@ FloatingActionButton(onPressed: () {} ,child: Image(image: AssetImage("image/ima
       ),
     );
   }
+Widget BulidItem(String colors,String images,String title)=>Padding(
+  padding: const EdgeInsets.only(right: 25,left: 25,top: 20),
+  child:   Container(decoration: BoxDecoration(
+    borderRadius: BorderRadius.circular(20),
+    color: Colors.white,
+
+  ),width: double.infinity,height: 62,child: Row(
+    children: [
+      SizedBox(width: 15,),
+      CircleAvatar(backgroundColor: HexColor("$colors"),child: Image(image: AssetImage("$images"),)),
+
+      SizedBox(width: 15,),
+      Text("$title",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16),),
+      Spacer(),
+ Image(image: AssetImage("image/image19.png")),
+      SizedBox(width: 20,),
+    ]
+
+    ,
+  ),),
+
+);
+
 }
